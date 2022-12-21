74b8155052d6609fef210dbdfeb10bb8522db8c0
Lucas Bordonal <lbordonal@gmail.com>
2022-12-21 13:54:56 +0000

    21/12/22 - 13:54

---
+++ minitalk_testes/includes_bonus/minitalk_bonus.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk_bonus.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbordona <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/21 12:55:36 by lbordona          #+#    #+#             */
+/*   Updated: 2022/12/21 13:25:52 by lbordona         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_BONUS_H
+# define MINITALK_BONUS_H
+
+# include "../libft/libft.h"
+# include <signal.h>
+# include <sys/types.h>
+
+#endif
---
+++ minitalk_testes/srcs/server.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbordona <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/07 20:57:49 by lbordona          #+#    #+#             */
+/*   Updated: 2022/12/21 13:12:35 by lbordona         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minitalk.h"
+
+static void	print_msg(int signal)
+{
+	static int	bit;
+	static int	character;
+
+	if (signal == SIGUSR1)
+		character += 1 << (7 - bit);
+	bit++;
+	if (bit == 8)
+	{
+		ft_putchar_fd(character, 1);
+		bit = 0;
+		character = 0;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	server_pid;
+
+	(void)argv;
+	server_pid = getpid();
+	if (argc != 1)
+	{
+		ft_putstr_fd("Please, verify the input → ./server", 1);
+		return (0);
+	}
+	ft_putstr_fd("PID: ", 1);
+	ft_putnbr_fd(server_pid, 1);
+	ft_putstr_fd("\n", 1);
+	signal(SIGUSR1, print_msg);
+	signal(SIGUSR2, print_msg);
+	while (1)
+		pause();
+	return (0);
+}
